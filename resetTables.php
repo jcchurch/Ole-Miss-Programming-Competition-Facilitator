c5037c3e8297ffbed68ac421ee55fc57d3ff62c4
James Church <jcchurch@gmail.com>
2011-10-03 19:50:38 -0500

    Added a quick script to reset the tables.

---
+++ resetTables.php
@@ -0,0 +1,14 @@
+<?
+
+die(); // Uncomment this line after executing. :)
+
+$competition_db = "competition.db";
+
+$db = new SQLiteDatabase($competition_db, 0666);
+
+$db->queryExec('DELETE FROM submissions;');
+$db->queryExec('DELETE FROM contestants;');
+
+echo "Thank you! Remember to comment out the 'die' command.\n\n"
+
+?>
