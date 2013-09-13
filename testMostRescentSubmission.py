1047bbdfb80ecc1ab30f1ebe8cd59039084ffb12
James Church <jcchurch@gmail.com>
2013-09-13 09:47:48 -0500

    testMostRecentSubmission.py not fully tested. Committing anyway.

--- testMostRescentSubmission.py
+++ testMostRescentSubmission.py
@@ -10,7 +10,6 @@ cur = db.cursor()
 cur.execute("SELECT contestant, problem, path FROM submissions WHERE status=-1 ORDER BY submitTime")
 for row in cur.fetchall():
     alltested = False 
-    
 
     print "Testing:",path
 
@@ -23,12 +22,27 @@ for row in cur.fetchall():
 
     status = 0
 
+    # C
     if type == "c":
-        pass
+        [status, output] = commands.getstatusoutput("gcc %s" % (newpath))
+        print "Compile status: ", status
+        print "Execution output: ", output
 
+        if status == 0:
+            [status, output] = commands.getstatusoutput("./%sa.out < %sProb%s/in > %smy.out" % (testarea, testarea, problem, testarea))
+            print "Execution status: ", status
+
+    # C++
     if type == "cpp":
-        pass
+        [status, output] = commands.getstatusoutput("g++ %s" % (newpath))
+        print "Compile status: ", status
+        print "Execution output: ", output
 
+        if status == 0:
+            [status, output] = commands.getstatusoutput("./%sa.out < %sProb%s/in > %smy.out" % (testarea, testarea, problem, testarea))
+            print "Execution status: ", status
+
+    # Java
     if type == "java":
         classpath = newpath.replace(".java","")
 
@@ -40,18 +54,32 @@ for row in cur.fetchall():
             [status, output] = commands.getstatusoutput("java %s < %sProb%s/in > %smy.out" % (classpath, testarea, problem, testarea))
             print "Execution status: ", status
 
+    # Lua
     if type == "lua":
-        pass
+        [status, output] = commands.getstatusoutput("lua %s < %sProb%s/in > %smy.out" % (newpath, testarea, problem, testarea))
+        print "Compile and execute status: ", status
 
-    if type == "scala":
-        pass
-
+    # Python 2 (not 3)
     if type == "py":
         [status, output] = commands.getstatusoutput("python %s < %sProb%s/in > %smy.out" % (newpath, testarea, problem, testarea))
         print "Compile and execute status: ", status
 
+    # Ruby
+    if type == "rb":
+        [status, output] = commands.getstatusoutput("ruby %s < %sProb%s/in > %smy.out" % (newpath, testarea, problem, testarea))
+        print "Compile and execute status: ", status
+
+    # Haskell
     if type == "hs":
-        pass
+        print "Haskell isn't on Turing."
+
+    # Scala 
+    if type == "scala":
+        print "Scala isn't on Turing."
+
+    ###############################################################
+    ############# The program is compiled and execute. ############
+    ###############################################################
 
     if status == 0:
         [status, output] = commands.getstatusoutput("diff %smy.out %sProb%s/out" % (testarea, testarea, problem))
